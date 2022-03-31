cdc0e70a760c06ef95a812ee9733211d5dfdc8a5
purplexpotato <88555558+purplexpotato@users.noreply.github.com>
2022-03-31 19:49:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function enter()
+{
+    window.location = "Healthy_food_for_dogs.html";
+}
+function small()
+{
+    window.location = "food_for_small_dogs.html";
+}
+function medium()
+{
+    window.location = "food_for_medium_dogs.html";
+}
+function Big()
+{
+    window.location = "food_for_big_dogs.html";
+}
